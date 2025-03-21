2253d0390e89a7fd7d826fe6f921893d3fbdc6d7
AzureAD\EliasNascimento <elias-a-nascimento@telecom.pt>
2025-03-21 17:08:10 +0000

    include new componentes Navbar, MovieCard and new Page Movie. Include new features to finallize the  project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+import "./MoviesGrid.css";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -12,7 +14,7 @@ const Home = () => {
 
     console.log(data);
 
-    setTopMovies(data);
+    setTopMovies(data.results);
   };
 
   useEffect(() => {
@@ -22,11 +24,13 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      {/* {topMovies &&
-        topMovies.map((movie) => {
-          <p>{movie.title}</p>;
-        })} */}
+    <div className="container">
+      <h2 className="title">Melhores Filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
     </div>
   );
 };
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+import "./MoviesGrid.css";
+
+const searchUrl = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Search = () => {
+  const [movies, setMovies] = useState([]);
+
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("q");
+
+  const getSearchedmovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    const searchWithQueryUrl = `${searchUrl}?${apiKey}&query=${query}`;
+
+    getSearchedmovies(searchWithQueryUrl);
+  }, [query]);
+
   return (
     <div>
-      <h1>Search</h1>
+      <div className="container">
+        <h2 className="title">
+          Resultados para: <span className="query-text">{query}</span>
+        </h2>
+      </div>
+      <div className="movies-container">
+        {movies.length === 0 && <p>Carregando...</p>}
+        {movies.length > 0 &&
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
     </div>
   );
 };
